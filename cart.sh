548fa84cedb8247b14a52b53651769b0a4256a1c
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-01 11:11:33 +0530

    editing cart

--- cart.sh
+++ cart.sh
@@ -3,13 +3,16 @@ dnf module enable nodejs:20 -y
 
 dnf install nodejs -y
 
-cp cart.service /etc/systemd/system/cart.service
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-
 cd /app
 unzip /tmp/cart.zip
+
+cp cart.service /etc/systemd/system/cart.service
+
+cd /app
 npm install
 
+systemctl daemon-reload
 
 systemctl enable cart
 systemctl start cart
