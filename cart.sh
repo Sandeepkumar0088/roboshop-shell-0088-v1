43d1de24c206ed1e1f47bc563de7764ac79c8f25
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-01 11:55:10 +0530

    editing cart

--- cart.sh
+++ cart.sh
@@ -4,9 +4,10 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
 useradd roboshop
+cp cart.service /etc/systemd/system/cart.service
+
 mkdir /app
 
-cp cart.service /etc/systemd/system/cart.service
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cd /app
 unzip /tmp/cart.zip
